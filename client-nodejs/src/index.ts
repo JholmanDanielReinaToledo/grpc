8949f2a6102621fe50811b7553e91e3d5e9fbabe
Jholman <jdreinat@geosat.com.co>
2024-09-10 20:35:04 -0500

    correccion de comando para que user la libreria recomendada y uso del create basico para ver el fncionamiento

--- client-nodejs/src/index.ts
+++ client-nodejs/src/index.ts
@@ -1,7 +1,27 @@
 import express, { Request, Response } from 'express';
-import { UserService } from './proto/user_grpc_pb';
+import { UserClient } from './proto/user_grpc_pb';
+const Grpc = require('@grpc/grpc-js');
+import { CreateUserRequest, UserModel } from './proto/user_pb';
 
-console.log(UserService)
+console.log(UserClient);
+const clientInsecureCreds = Grpc.credentials.createInsecure();
+const userService = new UserClient('localhost:8089', clientInsecureCreds)
+
+const user = new UserModel();
+user.setName("Juancito")
+user.setNumber("3213213211")
+
+const req = new CreateUserRequest()
+req.setUser(user)
+
+userService.create(req, (err, res) => {
+  console.log(err)
+  console.log(res.hasUser())
+  console.log(res.getUser()?.getId())
+  console.log(res.getUser()?.getName())
+  console.log(res.getUser()?.getNumber())
+  console.log(res.getRes()?.getCode())
+})
 
 const app = express();
 const PORT = process.env.PORT || 3000;
